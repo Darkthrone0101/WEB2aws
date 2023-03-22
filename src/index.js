077e29a3f1f738b367d9f5bd48702c7641d6fc60
Darkthrone <ja.vh.dev@gmail.com>
2023-03-22 16:53:46 -0500

    upladed & first commit for WEB2

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
+
+
+const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.set("view engine", "pug") 
+app.set("views", join( __dirname, "views" ))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => res.render("index"))
+app.use(express.static("views"))
+
+
+
+
+
+
+app.listen(3000)
+console.log("Server up in port", 3000)
